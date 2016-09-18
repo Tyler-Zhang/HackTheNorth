14bcb8047e24e31f93471e1f0676fbcd32ae4980
Tyler-Zhang <TZTylerzhang@gmail.com>
2016-09-17 20:11:20 -0400

    Finished login system

--- chrome/content.js
+++ chrome/content.js
@@ -1,9 +1,52 @@
 // content.js
-chrome.runtime.onMessage.addListener(
+/*chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       var firstHref = $("a[href^='http']").eq(0).attr("href");
       console.log(firstHref);
     }
   }
-);
+);*/
+
+console.log("I'm working!");
+var totalText = "";
+
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+	
+	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea') {
+		return;
+	}
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	totalText += textNode.nodeValue;
+}
+
+console.log(totalText);
